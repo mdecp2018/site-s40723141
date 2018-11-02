9ca2616397fb95c14c630b8e64a32c81d28a87bd
s40723141 <40723141@gm.nfu.edu.tw>
2018-11-02 16:20:19 +0800

    title change

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'KMOL'
-SITENAME = '你什麼時候產生了我沒有使用鏡花水月的錯覺(機械設計工程系 2018 Fall 分組網誌)'
+SITENAME = '                                            (機械設計工程系 2018 Fall 分組網誌)'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
